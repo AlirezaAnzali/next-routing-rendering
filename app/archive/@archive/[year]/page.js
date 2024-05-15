e000b28af146c32060a1be3dbb547076f5538b92
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2024-05-15 12:35:19 -0400

    Configuring Catch-All Routes

--- app/archive/@archive/[year]/page.js
+++
@@ -1,9 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
-import { notFound } from "next/navigation";
-
-export default function FilteredNewsPage({ params }) {
-  const newsYear = params.year;
-  const newsByYear = getNewsForYear(newsYear);
-  return <NewsList news={newsByYear} />;
-}
